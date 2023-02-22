9793dc8d14d17e35d0f492ce4165fd04522d8081
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-02-22 18:35:45 +0100

    refactor jest-puppeteer config file

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     launch: {
         dumpio: false,
-        headless: process.env.HEADLESS == 'true',   // export HEADLESS=='false' to launch Chromium
-        devtools: false,                               // optionally display devtools in non-headless mode
-        // slowMo: 1000,                                  // optionally slow down typing
-        defaultViewport: {                            // override default 800x600 pixel browser setting
+        headless: true,
+        devtools: false,    // optionally display devtools in non-headless mode
+        // slowMo: 1000,    // optionally slow down typing
+        defaultViewport: {  // override default 800x600 pixel browser setting
             width: 1024,
             height: 768
         }
